fff62684d130fd87e176cf4d7c24d620744e0aa3
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-04 17:44:48 +0100

    Esercizio finito
    Compresi i casi limite

--- js/script.js
+++ js/script.js
@@ -18,25 +18,51 @@ Dettagli
 5.consiglio: degli eventuali casi limte/controlli (es. voglio che sia aperto solo un dropdown alla volta, preoccupatevi alla fine).*/
 $(document).ready(function () {
 
+
   $('li.tariffazione').click(function () {
     $('.first').toggleClass('dbloc');
+  });
+  $('li.tariffazione').on("click", function(){
+    $('.second').removeClass('dbloc');
+    $('.third').removeClass('dbloc');
+    $('.fourth').removeClass('dbloc');
+    $('.fifth').removeClass('dbloc');
   });
 
   $('li.conti').click(function () {
     $('.second').toggleClass('dbloc');
+    $('.first').removeClass('dbloc');
+    $('.third').removeClass('dbloc');
+    $('.fourth').removeClass('dbloc');
+    $('.fifth').removeClass('dbloc');
+
   });
 
   $('li.prodotti').click(function () {
     $('.third').toggleClass('dbloc');
+    $('.first').removeClass('dbloc');
+    $('.second').removeClass('dbloc');
+    $('.fourth').removeClass('dbloc');
+    $('.fifth').removeClass('dbloc');
+
   });
 
   $('li.azienda').click(function () {
     $('.fourth').toggleClass('dbloc');
+    $('.first').removeClass('dbloc');
+    $('.second').removeClass('dbloc');
+    $('.third').removeClass('dbloc');
+    $('.fifth').removeClass('dbloc');
+
   });
 
   $('li.aiuto').click(function () {
     $('.fifth').toggleClass('dbloc');
-  });
+    $('.first').removeClass('dbloc');
+    $('.second').removeClass('dbloc');
+    $('.third').removeClass('dbloc');
+    $('.fourth').removeClass('dbloc');
 
+  });
 
 });
